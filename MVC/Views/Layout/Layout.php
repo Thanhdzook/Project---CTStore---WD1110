90dab9d88c18c9c9d43236f40226788532dded1c
bui cuong <bui230766@gmail.com>
2023-05-20 20:34:43 +0700

    test2

--- MVC/Views/Layout/Layout.php
+++ MVC/Views/Layout/Layout.php
@@ -8,10 +8,11 @@
   <link rel="stylesheet" href="/Project---CTStore---WD1110/MVC/wwwroot/bootstrap-5.0.2-dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="/Project---CTStore---WD1110/MVC/wwwroot/css/layout/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
-    <header class="Header">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top navbar-ed">
+    <header class="Header sticky-top">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-ed ">
           <div class="container">
             <a class="navbar-brand" href="#" style="color: #ffeba7;">CTstore</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,16 +21,16 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-              <a class="nav-link" href="#">Danh mục</a>
+              <a class="nav-link" href="#"><i class="bi bi-card-list"></i> Danh mục</a>
               </li>
             </ul>
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <form class="search-form d-flex" role="search" method="post" action="/Project---CTStore---WD1110/Show_MobilePhone/SreachMobilePhone/mobilePhone_name">
-                <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search" name="NameMobilePhone">
-                <button class="btn btn-outline-success search-btn" type="Sreach" name="Sreach">Search</button>
+                <input class="form-control me-2" type="text" placeholder="Bạn cần tìm gì?" aria-label="Search" name="NameMobilePhone">
+                <button class="btn btn-outline-success search-btn" type="Sreach" name="Sreach"><i class="bi bi-search"></i></button>
               </form>
               <li class="nav-item">
-              <a class="nav-link" href="/Project---CTStore---WD1110/Payment/ViewCart/null">Giỏ hàng</a>
+              <a class="nav-link" href="/Project---CTStore---WD1110/Payment/ViewCart/null"><i class="bi bi-cart"></i> Giỏ hàng</a>
               </li>
                         <?php
                           if(!isset($_SESSION["email"])){
@@ -46,11 +47,11 @@
                         if(isset($_SESSION["email"])){
                         ?>
                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tài khoản</a>
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person-circle"></i> Tài khoản</a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="/Project---CTStore---WD1110/Account/View_AccountInfo/<?php echo $_SESSION['email'];?>">Thông tin cá nhân</a></li>
+                  <li><a class="dropdown-item" href="/Project---CTStore---WD1110/Account/View_AccountInfo/<?php echo $_SESSION['email'];?>"><i class="bi bi-person-circle"></i> Thông tin cá nhân</a></li>
                   <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="/Project---CTStore---WD1110/Login_Sigin/Log_out">Đăng xuất</a></li>
+                  <li><a class="dropdown-item" href="/Project---CTStore---WD1110/Login_Sigin/Log_out"><i class="bi bi-box-arrow-right"></i> Đăng xuất</a></li>
                 </ul>
                 </li>
                         <?php
