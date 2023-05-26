b982163400a7e554ee57d42b2c1739bb9fe6e944
bui cuong <bui230766@gmail.com>
2023-05-26 23:56:49 +0700

    layout

--- MVC/Views/Layout/Layout.php
+++ MVC/Views/Layout/Layout.php
@@ -5,26 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Document</title>
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/Project---CTStore---WD1110/MVC/wwwroot/css/layout/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="/Project---CTStore---WD1110/MVC/wwwroot/css/layout/layout.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/Project---CTStore---WD1110/MVC/wwwroot/bootstrap-5.0.2-dist/css/bootstrap.min.css">
 </head>
 <body>
-<header class="header row">
-      <div class="logo col-md-3">
+
+<header class="header">
+      <div class="logo">
           <a href="/Project---CTStore---WD1110/Show_MobilePhone/ShowMobilePhone" class="logoC">CTSTORE</a>
       </div>
-      <div class="search col-md-4">
+      <div class="search">
         <form class="search-form d-flex" role="search" method="post" action="/Project---CTStore---WD1110/Show_MobilePhone/SreachMobilePhone_By_Name/mobilePhone_name">
           <input class="form-control" type="text" placeholder="Bạn cần tìm gì?" aria-label="Search" name="NameMobilePhone">
           <button class="btn btn-outline-success search-btn" type="Sreach" name="Sreach"><i class="bi bi-search"></i></button>
         </form>
       </div>
-      <div class="icons col-md-5">
+      <div class="icons">
 
         <a href="/Project---CTStore---WD1110/Payment/ViewCart/null" class="bi bi-basket3-fill"><span> Giỏ hàng</span></a>
+        <a href="#" class="bi bi-list" id="category"><span> Danh mục</span></a>
           <?php
             if(!isset($_SESSION["email"])){
           ?>
@@ -38,24 +39,24 @@
           if(isset($_SESSION["email"])){
           ?>
             <div class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" class="bi bi-person-circle">Tài khoản</a>
+              <a class="nav-link bi bi-person-circle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="margin-top:2px"><span> Tài khoản</span></a>
                 <ul class="dropdown-menu">
                   <?php
                     if($data["content"] != "Account"){
                   ?>
-                    <li><a class="dropdown-item bi bi-person-circle" href="/Project---CTStore---WD1110/Account/View_Account_Infor">Thông tin cá nhân</a></li>
+                    <li><a class="dropdown-item bi bi-person-circle" href="/Project---CTStore---WD1110/Account/View_Account_Infor"><span> Thông tin cá nhân</span></a></li>
                     <li><hr class="dropdown-divider"></li>
                   <?php
                   }
                   ?>
 
-                  <li><a class="dropdown-item bi bi-box-arrow-right" href="/Project---CTStore---WD1110/Login_Sigin/Log_out">Đăng xuất</a></li>
+                  <li><a class="dropdown-item bi bi-box-arrow-right" href="/Project---CTStore---WD1110/Login_Sigin/Log_out"><span> Đăng xuất</span></a></li>
                 </ul>
           </div>
           <?php
           }
           ?>
-        <a href="#" class="bi bi-list" id="category"><span> Danh mục</span></a>
+
       </div>
     </header>
     <div class="links">
@@ -77,7 +78,7 @@
             }
           ?>
 <script>
-    document.getElementsByClassName("bi")[4].addEventListener("click", function(){
+    document.getElementsByClassName("bi")[2].addEventListener("click", function(){
       document.getElementsByClassName("links")[0].classList.toggle("showmyLinks");
     });
 </script>
